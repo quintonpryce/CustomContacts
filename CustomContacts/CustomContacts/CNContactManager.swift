fee4bae088bd5384ce2dcd24c1467261e4953720
Quinton <quintonpryce@gmail.com>
2020-02-11 19:07:03 -0500

    Creating protocols for mockability and to abide by a contract

---
+++ CustomContacts/CustomContacts/CNContactManager.swift
@@ -0,0 +1,11 @@
+//
+//  CNContactManager.swift
+//  CustomContacts
+//
+//  Created by Quinton Pryce on 2020-02-11.
+//  Copyright © 2020 TN. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ CustomContacts/CustomContacts/CNContactManaging.swift
@@ -0,0 +1,33 @@
+//
+//  CNContactManaging.swift
+//  CustomContacts
+//
+//  Created by Quinton Pryce on 2020-02-11.
+//  Copyright © 2020 TN. All rights reserved.
+//
+import UIKit
+import Foundation
+import Contacts
+
+struct Contact {
+    struct PhoneNumber {
+        var label: String
+        var number: String
+    }
+    var firstName: String?
+    var lastName: String?
+    var numbers: [PhoneNumber]
+    var email: [String]?
+}
+
+protocol ContactManaging {
+    var contact: Contact { get }
+    func save()
+    func delete()
+
+    init(cnContact: CNContact)
+}
+
+protocol ContactViewController {
+    init(contactManager: ContactManaging)
+}
